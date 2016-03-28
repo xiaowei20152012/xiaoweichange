a50273a4c0afa1a9b3da5d808dd682d1c33e5372
unknown <liwei5@w-liwei.staff.ifeng.com>
2016-03-28 10:56:28 +0800

    20160328

---
+++ AndroidLeeChild/mygreendao/src/main/java/com/lee/mygreendao/dbhelpers/DbService.java
@@ -0,0 +1,13 @@
+package com.lee.mygreendao.dbhelpers;
+
+/**
+ * Created by liwei5 on 2016/3/25.
+ */
+public class DbService {
+    private static final String TAG = DbService.class.getSimpleName();
+    private static DbService instance;
+
+
+
+
+}
